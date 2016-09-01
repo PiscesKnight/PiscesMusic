fe7791d9c48c65745c94cebd5cd2860b231c90df
AndroidPlugin_Tai <ttainotee@163.com>
2016-09-01 09:13:58 +0800

    添加滑动切换页面功能，轮播图开源库

--- app/src/main/java/com/example/lintai/musicradio/MainActivity.java
+++ app/src/main/java/com/example/lintai/musicradio/MainActivity.java
@@ -6,20 +6,32 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.example.lintai.musicradio.adapter.FragmentAdapter;
+import com.example.lintai.musicradio.fragment.IndexFragment;
+import com.example.lintai.musicradio.fragment.OmnibusFragment;
+import com.example.lintai.musicradio.fragment.RadioStationFragment;
+import com.example.lintai.musicradio.fragment.RankingFragment;
+
 /**
  * Created by LinTai on 2016/8/30.
  */
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private DrawerLayout mDrawerLayout;
     private NavigationView navigationView;//菜单抽屉
     private View headerView;
+
+    private ViewPager viewPager;//切换页面
+    private TabLayout tabLayout;
 
     private ImageView userBtn;
 
@@ -34,12 +46,20 @@ public class MainActivity extends Activity implements View.OnClickListener{
         mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_main_drawer);
         navigationView = (NavigationView) findViewById(R.id.nv_main_navigation);
 
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
         userBtn = (ImageView) findViewById(R.id.user_btn);
 
 
         //获取抽屉头文件View
         headerView = navigationView.getHeaderView(0);
         setupDrawerContent(navigationView);
+
+        if (viewPager != null) {
+            setViewPager(viewPager);
+        }
+        tabLayout.setupWithViewPager(viewPager);
     }
 
 
@@ -84,6 +104,22 @@ public class MainActivity extends Activity implements View.OnClickListener{
                     }
                 }
         );
+    }
+
+
+    /**
+     * 设置ViewPager内容数据
+     *
+     * @param viewPager
+     */
+    private void setViewPager(ViewPager viewPager) {
+        FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager());
+        adapter.addFragemnt(new IndexFragment(), "首页");
+        adapter.addFragemnt(new OmnibusFragment(), "精选集");
+        adapter.addFragemnt(new RankingFragment(), "排行榜");
+        adapter.addFragemnt(new RadioStationFragment(), "电台");
+        viewPager.setAdapter(adapter);
+
     }
 
     @Override
